d084fc749275842f7369fedf3e0fd0becc324d9e
LIURLN <liurln38@gmail.com>
2018-08-25 10:41:45 +0700

    Modify run router reboot twice

--- src/script/reboot_router.sh
+++ src/script/reboot_router.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 python /home/geoscope/dev/geoscope-gateway/src/script/tplink_archer_c7_router_reset.py
+python /home/geoscope/dev/geoscope-gateway/src/script/tplink_archer_c7_router_reset.py
