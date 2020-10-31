ad5ba5fd537d278670eb9031a5cedbb1758456ad
Jesse R Codling <jcodling@andrew.cmu.edu>
2020-10-31 00:34:04 -0400

    Service Script - include Monitoring service

--- system/pignet.sh
+++ system/pignet.sh
@@ -3,4 +3,9 @@
 BASEDIR="$(dirname "`realpath "$0"`")"
 
 source $BASEDIR/../bin/activate
-python $BASEDIR/../src/main.py
+python $BASEDIR/../src/main.py &
+python $BASEDIR/../src/monitoring.py &
+
+wait
+
+trap 'jobs -p | xargs kill' EXIT INT QUIT TERM
