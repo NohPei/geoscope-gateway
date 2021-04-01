6d8d06072b8d7d778046762753a0d7b650825134
Jesse R Codling <jcodling@andrew.cmu.edu>
2021-04-01 12:56:13 -0400

    Logcheck for single stream rotating logs
    Adapts logcheck-pignet.sh to the single file logging process.

--- system/logcheck-pignet.sh
+++ system/logcheck-pignet.sh
@@ -24,11 +24,17 @@ if [ `find $STORAGE/data -type f -mmin -360 | wc -l` -gt 0 ]; then
 fi
 
 # find most recent logfiles to send
-STATUS_LOG="`ls $STORAGE/log/STATUS* -t | head -n1`"
 DEVICE_LOG="`ls $STORAGE/log/GEOSCOPE* -t | head -n1`"
+DEVICE_LOG_LAST="`ls $STORAGE/log/GEOSCOPE* -t | tail -n+2 | head -n1`"
+
+if [ -z $DEVICE_LOG_LAST ]; then # for when the log hasn't been rotated yet
+	DEVICE_LOG_LAST="$DEVICE_LOG"
+fi
 
 
-mail -S mta="$SENDMAIL" -S mta-arguments="$SENDMAIL_OPTS" -a "$STATUS_LOG" -a "$DEVICE_LOG" -s "$SUBJECT" "$TO" << EOF
+
+
+mail -S mta="$SENDMAIL" -S mta-arguments="$SENDMAIL_OPTS" -a "$DEVICE_LOG" -a "$DEVICE_LOG_LAST" -s "$SUBJECT" "$TO" << EOF
 From: $FROM
 
 Latest Geophone data is more than 6 hours old. There's probably a problem.
