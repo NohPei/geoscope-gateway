e22448d67db23b3418feab6e01ab86f73dc57262
Jesse R Codling <jclds139@gmail.com>
2021-05-13 13:42:34 -0400

    Geophone Data Archives
    To cope with some storage limiting the number of files, upload archives
    instead of the original folders.
    
    Doing bare-minimum compression during the archiving is actually faster
    than just storing due to how much data is in each day's folder.

---
+++ system/pigArchive.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -e
+
+if [ -z $PIGNET_DIR ]; then
+	PIGNET_DIR="/media/hdd/"
+fi
+
+archive_folder() {
+	zipfile="${1%/}.zip"
+	if [ -f "$zipfile" ] && ! 7z t "$zipfile" -stl; then
+		# if there's an existing archive, check it
+		rm -f "$zipfile"
+		# remove invalid old archives
+	fi
+
+	7z u "$zipfile" -stl -mx1 "$1"
+	if 7z t "$zipfile" -stl;  then
+		# verify the archive file
+		find "$1" -mtime +1 -delete
+		# if everything's fine, clear out old files (> 1 day) in the original directory
+	fi
+}
+
+while read origdir; do
+	archive_folder "$origdir" &
+done < <( find "$PIGNET_DIR/data" -maxdepth 1 -mindepth 1 -type d )
+
+wait
+
