1fb40f270bc3198a3805d03428e3f51c9604ac3c
Jesse R Codling <jclds139@gmail.com>
2021-12-20 15:07:36 -0500

    Asynchronous MQTT Client

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
 import logging
 import logging.handlers
 import time
-from multiprocessing import Process
-from mqtt_cli import mqtt_cli
-from monitoring import monitor_geophones
+import atexit
+import asyncio as aio
+from contextlib import AsyncExitStack
+from queue import SimpleQueue
+import uvloop
+import asyncio_mqtt as mqtt
+from sensor_logger import GeoAggregator
 
 
 ## Logging
@@ -16,67 +20,71 @@ file_log_handler = logging.handlers.TimedRotatingFileHandler(
 file_log_handler.setLevel(logging.INFO)
 
 formatter = logging.Formatter(
-    fmt="%(asctime)s %(levelname)s:%(name)s %(message)s", datefmt="%Y/%m/%d %I:%M:%S %p"
+    fmt="%(asctime)s %(levelname)s:%(name)s %(message)s", datefmt="%d%b%Y %H:%M:%S"
 )
 file_log_handler.setFormatter(formatter)
 
-logger.addHandler(file_log_handler)
-client_id_list = [
-    151,
-    152,
-    153,
-    154,
-    155,
-    156,
-    157,
-    158,
-    159,
-    160,
-    161,
-    162,
-    163,
-    164,
-    165,
-    166,
-    167,
-    168,
-    169,
-    170,
-    171,
-    172,
-    173,
-    174,
-    175,
-    176,
-    177,
-    178,
-    179,
-    180,
-    181,
-    182,
-    183,
-    184,
-    185,
-    186,
-    187,
-    188,
-    189,
-    190,
-]
+log_queue = SimpleQueue()
+logger.addHandler(logging.handlers.QueueHandler(log_queue))
+
+log_writer = logging.handlers.QueueListener(log_queue, file_log_handler,
+                                            respect_handler_level=True)
+atexit.register(log_writer.stop())
+log_writer.start()
+
+async def pignet():
+    async with AsyncExitStack as stack:
+        tasks = set()
+        aggregator = GeoAggregator(storage_root="/mnt/hdd/PigNet", log_name=logger.name+".Aggregator")
+
+        stack.push_async_callback(aggregator.flush) # during stack unwind, flush all in-progress data last
+
+        client_logger = logging.getLogger(logger.name + ".Client")
+
+        client = mqtt.Client("127.0.0.1", "18884", logger=client_logger, clean_session=False)
+        await stack.enter_async_context(client)
+
+        log_topics = ["$SYS/broker/log/E", "$SYS/broker/log/W"]
+        json_log_topics = ["geoscope/reply"]
+        sensor_topic_filter = "geoscope/node1/+"
+
+        for topic in log_topics:
+            manager = client.filtered_messages(topic)
+            messages = await stack.enter_async_context(manager)
+            task = aio.create_task(aggregator.log_status(messages))
+            tasks.add(task)
+            await client.subscribe(topic)
+
+        for topic in json_log_topics:
+            manager = client.filtered_messages(topic)
+            messages = await stack.enter_async_context(manager)
+            task = aio.create_task(aggregator.log_json_status(messages))
+            tasks.add(task)
+            await client.subscribe(topic)
+
+        sensor_manager = client.filtered_messages(sensor_topic_filter)
+        sensor_messages = stack.enter_async_context(sensor_manager)
+        sensor_task = aio.create_task(aggregator.log_sensors(sensor_messages))
+        tasks.add(task)
+        await client.subscribe(sensor_topic_filter)
+
+        await aio.gather(*tasks)
 
 
 
-def datalog():
-    sublog = logging.getLogger("GEOSCOPE.Subscriber")
 
-    sublog.info("## Starting data logging program")
-    mqtt_client = mqtt_cli(id_list=client_id_list, logger_name=sublog.name)
-    mqtt_client.start()
-    sublog.info("## End data capture.")
+async def maintain_mqtt(main_func):
+    reconnect_interval = 0.5
+    while True:
+        try:
+            await main_func()
+        except mqtt.MqttError as error:
+            await aio.to_thread(logger.critical,"Client Error \"%s\". Retrying in %f s.", error, reconnect_interval)
+        finally:
+            await aio.sleep(reconnect_interval)
+
 
 if __name__ == "__main__":
     logger.info("## Starting full capture system")
-    monitor = Process(target=monitor_geophones)
-    monitor.start()
-    datalog()
-    monitor.join()
+    uvloop.install()
+    aio.run(maintain_mqtt(pignet))
---
+++ src/sensor_logger.py
@@ -0,0 +1,87 @@
+import json
+import logging
+from random import randint
+import asyncio as aio
+from os import path as pathutil
+from datetime import datetime
+import aiofiles as files
+
+class GeoAggregator:
+    payloads = {}
+    background_tasks = set()
+
+    async def __init__(self, storage_root="/mnt/hdd/PigNet/", log_name="GEOSCOPE.Subscriber"):
+        self.root_path = storage_root
+        self.logger = logging.getLogger(log_name)
+
+    async def save_sensor_data(self, node_id):
+        save_time = datetime.now()
+        node_name = f"GEOSCOPE_SENSOR_{node_id}"
+        folder_path = pathutil.join(self.root_path, "data",
+                                    save_time.strftime("%Y-%m-%d"), node_name)
+        await files.os.makedirs(folder_path, exist_ok=True)
+
+        saved_payloads = self.payloads[node_id]
+        self.payloads[node_id].clear()
+
+        # Create json file
+        file_path = pathutil.join(folder_path, f"{save_time:'%Y-%m-%dT%H-%M-%S'}.json")
+        async with files.open(file_path, mode='w') as out_file:
+            await out_file.write(json.dumps(saved_payloads))
+        await aio.to_thread(self.logger.info,"[%s]: %s.json file created",
+                            node_name, file_path)
+
+
+        this_task = aio.current_task()
+        if this_task in self.background_tasks:
+            self.background_tasks.remove(this_task)
+
+
+    async def log_sensors(self, messages):
+        async for message in messages:
+            msg_time = datetime.now()
+            await aio.to_thread(self.logger.debug, "[%s] packet received")
+            node_id = message.topic.split('/')[-1]
+
+            try:
+                sensor_data = json.loads(message.payload.decode("utf-8"))
+            except json.decoder.JSONDecodeError:
+                await self.json_error_log(message.payload)
+                continue
+            sensor_data["timestamp"] = round(msg_time.timestamp()*1000)
+
+            if node_id not in self.payloads.keys():
+                self.payloads[node_id] = []
+
+            self.payloads[node_id].append(sensor_data)
+
+            if len(self.payloads[node_id]) >= randint(80, 120):
+                save_this_sensor_task = aio.create_task(self.save_sensor_data(node_id))
+                self.background_tasks.add(save_this_sensor_task)
+
+        await aio.gather(*self.background_tasks)
+
+
+
+
+    async def json_error_log(self, payload: str):
+            await aio.to_thread(self.logger.error,"Invalid JSON Packet:"
+                                "\n-----\n%s\n-----\n", payload)
+
+    async def log_json_status(self, messages):
+        async for message in messages:
+            try:
+                log_info = json.loads(message.payload.decode("utf=8"))
+                await aio.to_thread(self.logger.info,"[%s]: %s", log_info["uuid"], log_info["data"])
+            except json.decoder.JSONDecodeError:
+                await self.json_error_log(message.payload)
+
+
+    async def log_status(self, messages):
+        async for message in messages:
+            await aio.to_thread(self.logger.info, "[%s]: %s", message.topic, message.payload)
+
+    async def flush(self):
+        await aio.to_thread(self.logger.info,"Dumping In-Progress Sensor Data...")
+        for node in self.payloads:
+            await aio.shield(self.save_sensor_data(node))
