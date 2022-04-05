fbbe039604782bedb59470c73f232716da065659
Jesse R Codling <jclds139@gmail.com>
2022-04-04 22:47:07 -0400

    timesync: UDP Microseconds Broadcast
    Far from perfect, but should give a decent approximation for timesync
    when using ESP8266s which can't sync from the TSF

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,4 @@
 from .logger import *
 from .emulator import *
 from .pignet import *
+from . import timesync
--- src/timesync.py
+++ src/timesync.py
@@ -4,7 +4,7 @@ from io import IOBase
 import asyncio as aio
 import sliplib
 import periphery
-import asyncio_mqtt as mqtt
+import socket
 from .pignet import maintain_mqtt
 
 
@@ -46,3 +46,44 @@ async def timestampMicrosLoop(output_context_mgr, repeat_sec=1, interrupt_pin=No
             await aio.gather(*tasks) # start both tasks roughly simultaneously
             tasks.clear()
             await aio.sleep(repeat_sec)
+
+
+class UDPTimestampBroadcaster(aio.DatagramProtocol):
+    def __init__(self, repeat_sec, loop, disconn_future):
+        self.loop = loop
+        self.dead_flag = disconn_future
+        self.repeat_time = repeat_sec
+
+    def connection_made(self, transport):
+        self.transport = transport
+
+        # configure the underlying socket for broadcasting
+        raw_sock = transport.get_extra_info("socket")
+        raw_sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        self.send()
+
+    def connection_lost(self, exc):
+        self.dead_flag.set_result(True)
+
+    def send(self):
+        self.loop.call_later(self.repeat_time, self.send)
+        current_time = int(datetime.now().timestamp() * 1e6) % (2**64)
+            # get the current time as as 64-bit microseconds count
+        self.transport.sendto(current_time.to_bytes(8, byteorder='little'))
+        # send that timestamp as a 64b little-endian int
+
+async def UDPMicrosLoop(target_address='10.244.0.255', port = 2323, rep_sec = 1):
+    loop = aio.get_running_loop()
+    broadcaster_done = loop.create_future()
+    try:
+        endpoint_coro = loop.create_datagram_endpoint(lambda:
+                                                      UDPTimestampBroadcaster(rep_sec,
+                                                                              loop,
+                                                                              broadcaster_done),
+                                                      remote_addr=(target_address,
+                                                                   port),
+                                                      allow_broadcast=True)
+        transport, protocol = await endpoint_coro
+        await broadcaster_done
+    finally:
+        transport.close()
