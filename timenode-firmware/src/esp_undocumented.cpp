047b53a3228ac512d49c35e0f0030a7a75344dc9
Jesse R Codling <codling@umich.edu>
2023-02-09 22:57:06 -0500

    Match TimeNode to ESP32-C3 based v3 boards

--- timenode-firmware/src/esp_undocumented.cpp
+++
@@ -1,1 +0,0 @@
-#include "esp_undocumented.h"
--- timenode-firmware/src/timenode.ino
+++ timenode-firmware/src/timenode.ino
@@ -1,14 +1,6 @@
-#ifdef ESP8266
-# include<ESP8266WiFi.h>
-#include<ESPAsyncUDP.h>
-#else
 # include<WiFi.h>
-#include<AsyncUDP.h>
-#endif
+# include<esp_wifi.h>
 
-#include "esp_undocumented.h"
-
- 
 ///////////////////////////////////////////////////////////////////////////
 ////////////////////////// CONFIGURATION SECTION //////////////////////////
 ///////////////////////////////////////////////////////////////////////////
@@ -19,43 +11,22 @@ const char WIFI_PSK[] = "GoBucks!";
 #define SYNC_INTERRUPT digitalPinToInterrupt(D0)
 #define SERIAL_BAUD 115200
 
-#define TSF_SEND_INTERVAL_MS 1000
-#define TSF_SEND_PORT 2323
-//#define BROADCAST_TSF
-//uncomment to enable broadcasting the TSF regularly over UDP
 
 ///////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
-
-#ifdef BROADCAST_TSF
-# if defined(ESP8266)
-#  include<ESPAsyncUDP.h>
-# elif defined(ESP32)
-#  include<AsyncUDP.h>
-# else
-#  undef BROADCAST_TSF
-# endif
-#endif //defined(BROADCAST_TSF)
 
 
 volatile int64_t last_pulse_time;
 
-void IRAM_ATTR sync_pulse_isr(void) {
-	last_pulse_time = ESP_WDEV_TIMESTAMP();
+void ARDUINO_ISR_ATTR sync_pulse_isr(void) {
+	last_pulse_time = esp_wifi_get_tsf_time(WIFI_IF_STA);
 }
 
-#ifdef BROADCAST_TSF
-AsyncUDP time_broadcaster;
-uint32_t next_send_time_ms = 0;
-#endif //defined(BROADCAST_TSF)
 
 
 void setup() {
 	WiFi.mode(WIFI_STA);
-#ifdef ESP8266
-	WiFi.setPhyMode(WIFI_PHY_MODE_11N);
-#endif
 	WiFi.begin(WIFI_SSID, WIFI_PSK);
 
 	Serial.begin(SERIAL_BAUD);
@@ -79,14 +50,6 @@ void loop() {
 				break;
 		}
 	}
-
-#ifdef BROADCAST_TSF
-	if (millis() >= next_send_time_ms) {
-		next_send_time_ms = millis() + TSF_SEND_INTERVAL_MS;
-		int64_t time_to_send = ESP_WDEV_TIMESTAMP();
-		time_broadcaster.broadcastTo((uint8_t*) &time_to_send, sizeof time_to_send, TSF_SEND_PORT);
-	}
-#endif
 
 
 }
